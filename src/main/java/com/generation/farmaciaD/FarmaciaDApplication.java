40f31b07083868eaa5479eb3312cfe81ff248419
YancaDantas <yanca.d@live.com>
2024-07-22 13:06:02 -0300

    projeto

---
+++ src/main/java/com/generation/farmaciaD/FarmaciaDApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmaciaD/FarmaciaDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
